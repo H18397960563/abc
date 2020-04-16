893c9d2960461485ce77e8ee7fc4943d1d8a8b07
SunFlower <SunFlower@SunFlower>
2020-04-17 00:08:40 +0800

    解决冲突

--- src/test1/HelloWorld.java
+++ src/test1/HelloWorld.java
@@ -8,10 +8,7 @@ public static void main(String[] args) {
 	System.out.println("levelfirst");
 	System.out.println("adc go!!!");
 	System.out.println("dadadadada zou");
-<<<<<<< HEAD
 	System.out.println("hahaha");
-=======
->>>>>>> branch 'master' of https://github.com/H18397960563/abc.git
 	System.out.println("hehehehe");
 }
 }
