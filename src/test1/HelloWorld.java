2537d2b665a42ab1b4a957b6e1abfd81458092e8
SunFlower <SunFlower@SunFlower>
2020-04-16 23:57:53 +0800

    dadada

--- src/test1/HelloWorld.java
+++ src/test1/HelloWorld.java
@@ -7,6 +7,6 @@ public static void main(String[] args) {
 	System.out.println("hellochina");
 	System.out.println("levelfirst");
 	System.out.println("adc go!!!");
-	System.out.println("adcest1 zou");
+	System.out.println("dadadadada zou");
 }
 }
