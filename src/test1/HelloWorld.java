ed798c6e5308ed154eff522a733dca4ab7f24d1d
SunFlower <SunFlower@SunFlower>
2020-04-17 00:03:27 +0800

    解决冲突

--- src/test1/HelloWorld.java
+++ src/test1/HelloWorld.java
@@ -8,5 +8,7 @@ public static void main(String[] args) {
 	System.out.println("levelfirst");
 	System.out.println("adc go!!!");
 	System.out.println("dadadadada zou");
+	System.out.println("hahaha");
+	System.out.println("hehehehe");
 }
 }
